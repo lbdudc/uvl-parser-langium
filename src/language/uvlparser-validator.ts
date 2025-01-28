0ff98048871867d94002c825695718693fd01a68
Erick <erick.morales.pombo@udc.es>
2025-01-28 13:34:39 +0100

    Added feature duplication validation

--- src/language/uvlparser-validator.ts
+++ src/language/uvlparser-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { UvlparserAstType, FeatureModel } from './generated/ast.js';
+import type { UvlparserAstType, FeatureModel, Feature } from './generated/ast.js';
 import type { UvlparserServices } from './uvlparser-module.js';
 
 /**
@@ -9,7 +9,7 @@ export function registerValidationChecks(services: UvlparserServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.UvlparserValidator;
     const checks: ValidationChecks<UvlparserAstType> = {
-        FeatureModel: validator.checkFeatureModelValidity
+        FeatureModel: validator.checkFeatureModelValidity,
     };
     registry.register(checks, validator);
 }
@@ -20,6 +20,31 @@ export function registerValidationChecks(services: UvlparserServices) {
 export class UvlparserValidator {
 
     checkFeatureModelValidity(featureModel: FeatureModel, accept: ValidationAcceptor): void {
-    }
+        this.checkForFeatureDuplication(featureModel, accept);
+    };
 
+    checkForFeatureDuplication(featureModel: FeatureModel, accept: ValidationAcceptor): void {
+        const seenIds = new Set<string>();
+
+        const traverseFeature = (feature: Feature): void => {
+            if (feature.reference?.id) {
+                const id = feature.reference.id.toString();
+
+                if (seenIds.has(id)) {
+                    accept('error', `Duplicated feature ID: '${id}'`, {
+                        node: feature,
+                        property: 'reference'
+                    });
+                } else {
+                    seenIds.add(id);
+                }
+            }
+
+            feature.group?.forEach((group) => {
+                group.feature?.forEach(traverseFeature);
+            });
+        };
+
+        featureModel.features?.features.forEach(traverseFeature);
+    }
 }
